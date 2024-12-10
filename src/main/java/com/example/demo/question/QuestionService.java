18ced2e16b5ecd82b21582a9f71ce2153a653a26
tigerpoint123 <tigerrla@naver.com>
2024-12-10 16:54:41 +0900

    점프투스프링 2강 완료

--- src/main/java/com/example/demo/question/QuestionService.java
+++ src/main/java/com/example/demo/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.example.demo.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,5 +23,13 @@ public class QuestionService {
         if(question.isPresent()) {
             return question.get();
         } else throw new DataNotFoundException("question not found");
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }
