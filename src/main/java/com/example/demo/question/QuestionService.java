8b132958cc369cc26fd13a269f3b5cce586c70a5
tigerpoint123 <tigerrla@naver.com>
2024-12-10 12:39:40 +0900

    create question details, url mapping

--- src/main/java/com/example/demo/question/QuestionService.java
+++ src/main/java/com/example/demo/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.example.demo.question;
 
+import com.example.demo.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,5 +14,13 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(int id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+
+        if(question.isPresent()) {
+            return question.get();
+        } else throw new DataNotFoundException("question not found");
     }
 }
