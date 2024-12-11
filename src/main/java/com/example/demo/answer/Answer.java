60f06796206ae935f46c9e9992ea91abb3d13fae
tigerpoint123 <tigerrla@naver.com>
2024-12-11 15:51:04 +0900

    jump to spring 3 -10 recommend

--- src/main/java/com/example/demo/answer/Answer.java
+++ src/main/java/com/example/demo/answer/Answer.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -29,4 +30,6 @@ public class Answer {
 
     private LocalDateTime modifyDate;
 
+    @ManyToMany
+    Set<SiteUser> voter;
 }
--- src/main/java/com/example/demo/question/Question.java
+++ src/main/java/com/example/demo/question/Question.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -33,4 +34,7 @@ public class Question {
     private SiteUser author;
 
     private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }
